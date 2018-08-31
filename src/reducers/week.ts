4aadc8f1522739e3e5458729ebfc7439e19d57b2
Stradler) <eamalakhov@gmail.com>
2018-08-31 13:37:23 +0300

    add jest config and move generateWeek to separte file with helpers with import

--- src/reducers/week.ts
+++ src/reducers/week.ts
@@ -1,13 +1,4 @@
-export function generateWeek(): string[]{
-    const today: Date = new Date();
-    const week: string[] = [];
-    for (let i: number = 0; i < 7; i += 1) {
-        const tempDay: Date = new Date(today);
-        tempDay.setDate(today.getDate() + i);
-        week.push(`${tempDay.getDate()}.${tempDay.getMonth()}.${tempDay.getFullYear()}`);
-      }
-    return week;
-}
+import {generateWeek} from "../helpers/misc";
 
 
 export const week = (state = generateWeek(), action = {}): string[] => {
